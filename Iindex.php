cce1332e709e04928e8c69da1b049c3796fb9af3
sourav199 <souravdhar1999@gmail.com>
2022-02-28 12:49:31 -0500

    done with part 1

--- Iindex.php
+++ Iindex.php
@@ -1,4 +1,38 @@
-<?php
-echo "Hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HW3</title>
+</head>
+<body>
+    <?php
+    print("PART 1");
+    echo "<BR>";
+    echo "<BR>";
+    echo "<BR>";
+    print("");
 
-?>
+    function isBitten($dayIate, $probability)
+    {
+        $ans = mt_rand(1, $dayIate)>=$probability;
+        if($ans==1)
+        {print("Cgarlie ate my launch!");}
+        else {
+            print("Charlie did not eat my launch!");
+        }
+      
+    }
+    isBitten(100, 50);
+
+ 
+   
+
+
+ 
+
+    ?>
+    
+</body>
+</html>
