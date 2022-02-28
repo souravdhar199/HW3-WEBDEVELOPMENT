c82a9eae629024064b5a7d65e2fc3d5c0b1fbef5
sourav199 <souravdhar1999@gmail.com>
2022-02-28 14:18:28 -0500

    part 3 done

--- Iindex.php
+++ Iindex.php
@@ -57,6 +57,46 @@
 ?>
 </table>
 
+<?php
+  echo "<BR>";
+  print("PART 3");
+  echo "<BR>";
+  echo "<BR>";
+
+  $month = array ('January', 'February', 'March', 'April',
+ 'May', 'June', 'July', 'August',
+ 'September', 'October', 'November', 'December');
+  
+  print_r($month);
+  echo "<BR>";
+  $size = count($month);
+ echo"<p> Before sorting (Standard for loop) </p>";
+ for($i=0;$i<$size; $i++){
+     echo $month[$i];
+     echo "<br>";
+ }
+ echo"<p>After sorting</p>";
+ $temp = $month;
+ sort($temp);
+ for($i=0;$i<$size; $i++){
+    echo $temp[$i];
+    echo "<br>";
+}
+echo"<p> Before sorting (foreach) </p>";
+foreach($month as $m){
+    print($m);
+    echo "<br>";
+}
+
+sort($month);
+echo"<p> After sorting (foreach) </p>";
+foreach($month as $m){
+    print($m);
+    echo "<br>";
+}
+
+?>
+
 
 
     
